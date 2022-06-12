7377efca02594f7389dd3d23bca5712f340f3054
PeteC88 <pietrociccarello@gmail.com>
2022-06-12 11:20:29 +0200

    babel file commented

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
+  //run npm i babel-plugin-transform-remove-console --save-dev and added this "env" to delete the console log for build production
   "env": {
     "production": {
       "plugins": ["transform-remove-console"]
